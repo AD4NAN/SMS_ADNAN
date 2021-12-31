f90dac0bd8475e03a72274dbaa94252310161dc3
Adnan Islam <95557642+AD4NAN@users.noreply.github.com>
2021-12-31 19:58:29 +0600

    Update cn.py

--- cn.py
+++ cn.py
@@ -19,4 +19,4 @@ yellow="\033[0;33m"       # Yellow
 
 #cn.py
 
-print(red+"##################\n =============================\n Creatot : HridoY Ahammed Nirob (Hacker)\n =============================\n Facebook : www.facebook.com/Adnan.Islam.0.2\n =============================\n Modifier : Adnan Islam\n =============================\n GITHUB : https://github.com/AD4NAN\n =============================\n FB Page: www.facebook.com/Adnan.Islam\n==========================================================")
+print(red+"##################\n =============================\n Creatot : ADNAN ISLAM    \n =============================\n Facebook : www.facebook.com/Adnan.Islam.0.2\n =============================\n Modifier : Adnan Islam\n =============================\n GITHUB : https://github.com/AD4NAN\n =============================\n FB Page: www.facebook.com/Adnan.Islam\n==========================================================")
